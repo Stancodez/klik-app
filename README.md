d2c213bac4661abaa80e639c9f7688d06dc1cfc8
Stanley <stanleyemeke71@gmail.com>
2024-10-09 17:39:51 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-Webstack - Portfolio Project - Pitch  
-KLIK APP
--- server/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-exports.getMe = async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id);
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
